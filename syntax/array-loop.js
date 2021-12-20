0d628f6a5267e3bfb1a06ddf3726451593088bfc
BaekJiwan <95860566+BaekJiwan@users.noreply.github.com>
2021-12-21 01:26:02 +0900

    Add files via upload

---
+++ syntax/array-loop.js
@@ -0,0 +1,11 @@
+var number = [1,400,12,34,5];
+var total=0;
+var i=0;
+number.push(39,100)
+while(i<number.length){
+  console.log(number[i])
+  total += number[i];
+  i += 1;
+};
+
+console.log(`total: ${total}`);
